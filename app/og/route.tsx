8874ff492b25f17f90561183affb89230e325991
Rwin <rwinzhao@gmail.com>
2023-09-25 13:31:05 +1300

    op-bg.png to jpg

--- app/og/route.tsx
+++ app/og/route.tsx
@@ -21,7 +21,7 @@ export async function GET(req: NextRequest) {
           flexDirection: "column",
           alignItems: "flex-start",
           justifyContent: "center",
-          backgroundImage: "url(https://bitly.dev/og-bg.png)",
+          backgroundImage: "url(https://bitly.dev/og-bg.jpg)",
         }}
       >
         <div
